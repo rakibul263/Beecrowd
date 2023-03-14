27b8db81ce36354cbec565b97cd0e354ae1a2e61
Md Rakibul Hasan <121483344+rakibul263@users.noreply.github.com>
2023-03-14 10:39:33 +0600

    Add files via upload

---
+++ 1546.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+    while(1)
+    {
+        int N;
+        scanf("%d", &N);
+        {
+            if(N==1)printf("Rolien\n");
+            else if(N==2)printf("Naej\n");
+            else if(N==3)printf("Elehcim\n");
+            else printf("Odranoel\n");
+
+
+        }
+
+    }
+    return 0;
+}
